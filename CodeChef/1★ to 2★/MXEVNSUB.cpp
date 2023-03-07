cb06b3ce029faa4acb5dc3245324bfedf8b13369
Klosepa9p <Yusuf_kndl-2002@hotmail.com>
2023-03-07 20:25:31 +0300

    last_question_on_2

--- CodeChef/1★ to 2★/MXEVNSUB.cpp
+++ CodeChef/1★ to 2★/MXEVNSUB.cpp
@@ -1,57 +1,21 @@
 #include <iostream>
 
-/*
 int main(void)
 {
+	
 	int		T;
-	int		N;
-	int		control;
-		
-	std::cin >> T;
-	while (T--)
-	{
-		std::cin >> N;
-		if (N % 2 == 1)
-		{
-			control = (N + 1) / 2;
-			if (control % 2 == 0)
-				std::cout << N << std::endl;
-			else
-				std::cout << N - 1 << std::endl;
-		}
-		else
-		{
-			control = N / 2;
-			if (control % 2 == 0)
-				std::cout << N << std::endl;
-			else
-				std::cout << N - 2 << std::endl;
-		}
-	}
-	return (0);	
-} */
-
-#include <iostream>
-int main(void)
-{		
-	int		T;
-	int		N;
+	int 	N;
 	int		sum;
 	
 	std::cin >> T;
 	while (T--)
 	{
 		std::cin >> N;
-		sum = (N * (N + 1) / 2);
+		sum = (N * (N + 1)) / 2;
 		if (sum % 2 == 0)
 			std::cout << N << std::endl;
 		else
-		{
-			if (N % 2 == 0)
-				std::cout << N - 2 << std::endl;
-			else
-				std::cout << N - 1 << std::endl;
-		}	
+			std::cout << N - 1 << std::endl;
 	}
 	return (0);
 }
